cfeabb7de0ab51082416d58a60adfd12e42d6179
Christos Nikolaou <me@christosnikolaou.org>
2023-08-26 12:34:29 +0300

    Add API route `filter`; get filtered list of accounts

---
+++ pages/api/filter.ts
@@ -0,0 +1,20 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+import { getAccountFilteredData, FilteredAccountData } from '@/lib/account-data';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<FilteredAccountData[]>,
+) {
+  if (req.method === 'POST') {
+    try {
+      const filteredData = await getAccountFilteredData(JSON.parse(req.body));
+      console.log(filteredData);
+      res.status(200).json(filteredData);
+    } catch (error) {
+      res.status(400);
+    }
+  } else {
+    res.status(400);
+  }
+}
