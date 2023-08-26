8236f7c91687ff9e4987572056ab484bc548334b
Christos Nikolaou <me@christosnikolaou.org>
2023-08-26 09:01:13 +0300

    Add credit data stored in Union Protocol to Home page
    Add the credit-related data that is stored in the Union Protocol
    to the Hope page of the application.

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,14 @@ export default function Home({ accountData }) {
     <>
       <h1>Credit Source</h1>
       {accountData.map((acc) => (
-        <h2>{acc.address.toUpperCase()}</h2>
+        <div className="card">
+          <h2>{acc.address.toUpperCase()}</h2>
+          <p>Trusted by: {acc.creditData.ethereum.trustedBy} accounts in Ethereum</p>
+          <p>Trusted by: {acc.creditData.arbitrum.trustedBy} accounts in Arbitrum</p>
+          <hr />
+          <p>Trusted Credit in Ethereum: {acc.creditData.ethereum.trustedCredit}</p>
+          <p>Trusted Credit in Arbitrum: {acc.creditData.arbitrum.trustedCredit}</p>
+        </div>
       ))}
     </>
   );
