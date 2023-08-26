f4bd9ed1b55e2195e2df9216267114441529745b
Christos Nikolaou <me@christosnikolaou.org>
2023-08-26 12:07:29 +0300

    Add function to fetch token contract data from Alchemy
    Add function to fetch token contract data from Alchemy for various
    blockchains.

--- lib/token.ts
+++ lib/token.ts
@@ -1,4 +1,4 @@
-import { Network, Alchemy } from 'alchemy-sdk';
+import { Network, Alchemy, TokenBalancesResponse } from 'alchemy-sdk';
 import { utils } from 'ethers';
 
 const settingsEthereum = {
@@ -22,6 +22,13 @@ const alchemyOptimism = new Alchemy(settingsOptimism);
 
 type UserAddress = string;
 
+export type Chain = 'Ethereum' | 'Arbitrum' | 'Optimism';
+
+type TokenContractAddress = {
+  address: string;
+  chain: Chain;
+};
+
 export type AccountCurrentBalance = {
   eth: string;
   arb: string;
@@ -43,4 +50,94 @@ export async function getBalance(userAddress: UserAddress): Promise<AccountCurre
   };
 
   return networkBalances;
+}
+
+export type AccountCurrentTokenBalance = {
+  tokenAddress: string;
+  chain: Chain;
+  balance: number;
+};
+
+function processTokenBalancesResponse(
+  tokenBalances: TokenBalancesResponse,
+  chain: Chain,
+): AccountCurrentTokenBalance[] {
+  // Post process data token data received by the Alchemy API
+
+  const data = tokenBalances.tokenBalances
+    .filter((balance) => balance.tokenBalance !== null)
+    .map((balance) => {
+      return {
+        tokenAddress: balance.contractAddress,
+        chain: chain,
+        balance: parseFloat(utils.formatEther(balance.tokenBalance!)),
+      };
+    });
+
+  return data;
+}
+
+export async function getTokenBalance(
+  userAddress: UserAddress,
+  tokenContractAddresses: TokenContractAddress[],
+): Promise<AccountCurrentTokenBalance[]> {
+  // Split the token requests based on the chain that the contracts refer to
+  // fetch the data for each chain from Alchemy API and then
+  // accumulate the data into the result array
+
+  const ethereumTokens = tokenContractAddresses
+    .filter((address) => address.chain === 'Ethereum')
+    .map((addressData) => addressData.address);
+  const arbitrumTokens = tokenContractAddresses
+    .filter((address) => address.chain === 'Arbitrum')
+    .map((addressData) => addressData.address);
+  const optimismTokens = tokenContractAddresses
+    .filter((address) => address.chain === 'Optimism')
+    .map((addressData) => addressData.address);
+
+  let result: AccountCurrentTokenBalance[] = [];
+
+  if (ethereumTokens.length > 0) {
+    try {
+      const tokenBalances = await alchemyEthereum.core.getTokenBalances(
+        userAddress,
+        ethereumTokens,
+      );
+      const data = processTokenBalancesResponse(tokenBalances, 'Ethereum');
+      result = [...result, ...data];
+    } catch (error) {
+      console.error(error);
+      throw new Error('Not valid contract_addresses');
+    }
+  }
+
+  if (arbitrumTokens.length > 0) {
+    try {
+      const tokenBalances = await alchemyArbitrum.core.getTokenBalances(
+        userAddress,
+        arbitrumTokens,
+      );
+      const data = processTokenBalancesResponse(tokenBalances, 'Arbitrum');
+      result = [...result, ...data];
+    } catch (error) {
+      console.error(error);
+      throw new Error('Not valid contract_addresses');
+    }
+  }
+
+  if (optimismTokens.length > 0) {
+    try {
+      const tokenBalances = await alchemyOptimism.core.getTokenBalances(
+        userAddress,
+        optimismTokens,
+      );
+      const data = processTokenBalancesResponse(tokenBalances, 'Optimism');
+      result = [...result, ...data];
+    } catch (error) {
+      console.error(error);
+      throw new Error('Not valid contract_addresses');
+    }
+  }
+
+  return result;
 }
