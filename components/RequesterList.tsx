bcc2882ba2ce4bbaf99c13e7edfe60c29ecd95f1
Vasilis Nikolaou <vasilis.n.nikolaou@gmail.com>
2023-08-28 02:01:45 +0300

    Add `RequesterCard` component for requester styling

--- components/RequesterList.tsx
+++ components/RequesterList.tsx
@@ -1,3 +1,5 @@
+import RequesterCard from '@/components/RequesterCard';
+
 import { AccountData, FilteredAccountData } from '@/lib/account-data';
 
 interface RequesterListProps {
@@ -8,30 +10,7 @@ export default function RequesterList({ data }: RequesterListProps) {
   return (
     <>
       {data.map((acc) => (
-        <div className="card" key={acc.address}>
-          <h2>{acc.address.toUpperCase()}</h2>
-          <p>Requested Credit: {acc.requestedCredit} DAI</p>
-          <p>Trusted by: {acc.creditData.ethereum.trustedBy} accounts in Ethereum</p>
-          <p>Trusted by: {acc.creditData.arbitrum.trustedBy} accounts in Arbitrum</p>
-          <hr />
-          <p>Trusted Credit in Ethereum: {acc.creditData.ethereum.trustedCredit}</p>
-          <p>Trusted Credit in Arbitrum: {acc.creditData.arbitrum.trustedCredit}</p>
-          <hr />
-          <p>{acc.currentBalance.eth} ETH</p>
-          <p>{acc.currentBalance.arb} ARB</p>
-          <p>{acc.currentBalance.op} OP</p>
-          {'tokenBalances' in acc && (
-            <>
-              <hr />
-              <h3>Token Balances</h3>
-              {acc.tokenBalances?.map((token) => (
-                <p>
-                  Contract {token.tokenAddress} has {token.balance}
-                </p>
-              ))}
-            </>
-          )}
-        </div>
+        <RequesterCard data={acc} key={acc.address} />
       ))}
     </>
   );
