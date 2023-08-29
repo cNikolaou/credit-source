5ee6b98c0ea42210fdd442f017a6bae5f528955b
Christos Nikolaou <me@christosnikolaou.org>
2023-08-29 18:01:04 +0300

    Remove and deprecate secrets

--- contract-hardhat/hardhat.config.js
+++ contract-hardhat/hardhat.config.js
@@ -1,7 +1,7 @@
 require('@nomicfoundation/hardhat-toolbox');
 
-const ALCHEMY_API_KEY_ARBITRUM = 'YSy9bkhUEuOcKS2P3D5zDElA5O5apuOA';
-const GOERLI_PRIVATE_KEY = '36e82bda23fb9afaecd248152b7b0f09cf16a315cc90f920d11b112402a5ed4a';
+const ALCHEMY_API_KEY_ARBITRUM = process.env.ALCHEMY_API_KEY;
+const EVM_PRIVATE_KEY = process.env.EVM_DEV_PRIV_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -9,12 +9,13 @@ module.exports = {
   networks: {
     goerli: {
       url: `https://arb-goerli.g.alchemy.com/v2/${ALCHEMY_API_KEY_ARBITRUM}`,
-      accounts: [GOERLI_PRIVATE_KEY],
+      accounts: [EVM_PRIVATE_KEY],
     },
     arbitrumGoerli: {
       url: 'https://goerli-rollup.arbitrum.io/rpc',
       chainId: 421613,
-      accounts: [GOERLI_PRIVATE_KEY],
+      accounts: [EVM_PRIVATE_KEY],
+    },
     },
   },
 };
